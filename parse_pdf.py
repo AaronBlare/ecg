a9a6ca542d3ca083be0ac4f3f86040966724e36d
Aaron Blare <alena.kalyakulina@itmm.unn.ru>
2020-11-02 23:22:03 +0300

    Parse pdf fixes

--- parse_pdf.py
+++ parse_pdf.py
@@ -6,7 +6,7 @@ from pdfminer.high_level import extract_text
 from tabula import read_pdf
 
 path = get_path() + '/data/pdf/'
-data_dict = {'code': [], 'name': [], 'ecg_date': [], 'birth_date': [], 'age': [],
+data_dict = {'code': [], 'name': [], 'ecg_date': [], 'ecg_time': [], 'birth_date': [], 'age': [],
              'sex': [], 'height': [], 'weight': [], 'heart_rate': []}
 
 for file in os.listdir(path):
@@ -31,8 +31,9 @@ for file in os.listdir(path):
 
     if text_list[4] == 'Case No.:':
 
-        ecg_date = text_list[5]
-        data_dict['ecg_date'].append(ecg_date)
+        ecg_date = text_list[5].split(' ')
+        data_dict['ecg_date'].append(ecg_date[0])
+        data_dict['ecg_time'].append(ecg_date[2])
 
         birth_date = text_list[6]
         data_dict['birth_date'].append(birth_date)
@@ -104,9 +105,181 @@ for file in os.listdir(path):
             else:
                 data_dict[axis_names_full[i]] = [axis[axis_names[i]]]
 
+    elif text_list[3] == 'Case No.:':
+
+        name = ''
+        data_dict['name'][-1] = name
+        code = text_list[2]
+        data_dict['code'][-1] = code
+
+        ecg_date = text_list[4].split(' ')
+        data_dict['ecg_date'].append(ecg_date[0])
+        data_dict['ecg_time'].append(ecg_date[2])
+
+        birth_date = text_list[5]
+        data_dict['birth_date'].append(birth_date)
+
+        age = text_list[6].split(' ')[0]
+        try:
+            int(age)
+            data_dict['age'].append(int(age))
+        except ValueError:
+            data_dict['age'].append('nan')
+
+        sex = text_list[7]
+        data_dict['sex'].append(sex)
+
+        height = text_list[8].split(' ')[0]
+        try:
+            float(height)
+            data_dict['height'].append(float(height))
+        except ValueError:
+            data_dict['height'].append('nan')
+
+        weight = text_list[9].split(' ')[0]
+        try:
+            float(weight)
+            data_dict['weight'].append(float(weight))
+        except ValueError:
+            data_dict['weight'].append('nan')
+
+        heart_rate = text_list[20].split(' ')[0]
+        try:
+            int(heart_rate)
+            data_dict['heart_rate'].append(int(heart_rate))
+        except ValueError:
+            data_dict['heart_rate'].append('nan')
+
+        intervals_names = text_list[22:28]
+        intervals = {key: '' for key in intervals_names}
+        for i in range(28, 34):
+            curr_interval_value = text_list[i]
+            try:
+                int(curr_interval_value.split(' ')[0])
+                intervals[text_list[i - 6]] = int(curr_interval_value.split(' ')[0])
+            except ValueError:
+                intervals[text_list[i - 6]] = 'nan'
+
+        intervals_names_full = []
+        for i in range(0, len(intervals_names)):
+            intervals_names_full.append('interval_' + intervals_names[i])
+            if intervals_names_full[i] in data_dict:
+                data_dict[intervals_names_full[i]].append(intervals[intervals_names[i]])
+            else:
+                data_dict[intervals_names_full[i]] = [intervals[intervals_names[i]]]
+
+        axis_names = text_list[36:39]
+        axis = {key: '' for key in axis_names}
+        for i in range(39, 42):
+            curr_axis_value = text_list[i]
+            try:
+                int(curr_axis_value.split(' ')[0])
+                axis[text_list[i - 3]] = int(curr_axis_value.split(' ')[0])
+            except ValueError:
+                axis[text_list[i - 3]] = 'nan'
+
+        axis_names_full = []
+        for i in range(0, len(axis_names)):
+            axis_names_full.append('axis_' + axis_names[i])
+            if axis_names_full[i] in data_dict:
+                data_dict[axis_names_full[i]].append(axis[axis_names[i]])
+            else:
+                data_dict[axis_names_full[i]] = [axis[axis_names[i]]]
+
+    elif text_list[0] != 'Pat-Name:':
+
+        name_list = re.findall(r"[\w']+", text_list[0].encode("latin-1").decode('cp1251'))
+        if len(name_list) == 3:
+            if len(name_list[1]) > len(name_list[-1]):
+                name = name_list[0] + ' ' + name_list[-1] + ' ' + name_list[1]
+            else:
+                name = name_list[0] + ' ' + name_list[1] + ' ' + name_list[-1]
+        elif len(name_list) == 2:
+            name = name_list[0] + ' ' + name_list[1]
+        else:
+            name = name_list[0]
+        data_dict['name'][-1] = name
+        code = text_list[1].encode("latin-1").decode('cp1251')
+        data_dict['code'][-1] = code
+
+        ecg_date = text_list[41].split(' ')
+        data_dict['ecg_date'].append(ecg_date[0])
+        data_dict['ecg_time'].append(ecg_date[2])
+
+        birth_date = text_list[2]
+        data_dict['birth_date'].append(birth_date)
+
+        age = text_list[3].split(' ')[0]
+        try:
+            int(age)
+            data_dict['age'].append(int(age))
+        except ValueError:
+            data_dict['age'].append('nan')
+
+        sex = text_list[4]
+        data_dict['sex'].append(sex)
+
+        height = text_list[5].split(' ')[0]
+        try:
+            float(height)
+            data_dict['height'].append(float(height))
+        except ValueError:
+            data_dict['height'].append('nan')
+
+        weight = text_list[6].split(' ')[0]
+        try:
+            float(weight)
+            data_dict['weight'].append(float(weight))
+        except ValueError:
+            data_dict['weight'].append('nan')
+
+        heart_rate = text_list[19].split(' ')[0]
+        try:
+            int(heart_rate)
+            data_dict['heart_rate'].append(int(heart_rate))
+        except ValueError:
+            data_dict['heart_rate'].append('nan')
+
+        intervals_names = text_list[21:27]
+        intervals = {key: '' for key in intervals_names}
+        for i in range(27, 33):
+            curr_interval_value = text_list[i]
+            try:
+                int(curr_interval_value.split(' ')[0])
+                intervals[text_list[i - 6]] = int(curr_interval_value.split(' ')[0])
+            except ValueError:
+                intervals[text_list[i - 6]] = 'nan'
+
+        intervals_names_full = []
+        for i in range(0, len(intervals_names)):
+            intervals_names_full.append('interval_' + intervals_names[i])
+            if intervals_names_full[i] in data_dict:
+                data_dict[intervals_names_full[i]].append(intervals[intervals_names[i]])
+            else:
+                data_dict[intervals_names_full[i]] = [intervals[intervals_names[i]]]
+
+        axis_names = text_list[35:38]
+        axis = {key: '' for key in axis_names}
+        for i in range(38, 41):
+            curr_axis_value = text_list[i]
+            try:
+                int(curr_axis_value.split(' ')[0])
+                axis[text_list[i - 3]] = int(curr_axis_value.split(' ')[0])
+            except ValueError:
+                axis[text_list[i - 3]] = 'nan'
+
+        axis_names_full = []
+        for i in range(0, len(axis_names)):
+            axis_names_full.append('axis_' + axis_names[i])
+            if axis_names_full[i] in data_dict:
+                data_dict[axis_names_full[i]].append(axis[axis_names[i]])
+            else:
+                data_dict[axis_names_full[i]] = [axis[axis_names[i]]]
+
     else:
-        ecg_date = text_list[41]
-        data_dict['ecg_date'].append(ecg_date)
+        ecg_date = text_list[41].split(' ')
+        data_dict['ecg_date'].append(ecg_date[0])
+        data_dict['ecg_time'].append(ecg_date[2])
 
         birth_date = text_list[4]
         data_dict['birth_date'].append(birth_date)
